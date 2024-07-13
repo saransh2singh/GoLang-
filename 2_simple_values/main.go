d51ae9a1862649b46f0ae216f762e795049d431f
Saransh Singh <saransh2singh@gmail.com>
2024-07-13 20:54:12 +0530

    Simple Values

---
+++ 2_simple_values/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// int
+	fmt.Println(1 + 1)
+	// string
+	fmt.Println("Hi")
+	// bool
+	fmt.Println(true)
+	// floats
+	fmt.Println(10.5)
+	fmt.Println(10.5 / 2.5)
+}
