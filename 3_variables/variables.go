0d4d0e31963686fbc4c3d7e77358e79fc79388d0
Saransh Singh <saransh2singh@gmail.com>
2024-07-13 21:10:42 +0530

    Variables

---
+++ 3_variables/variables.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "hi"
+	// var name = "hello"
+
+	// var isAdult = true
+	// var isAdult bool = true
+
+	// var age int = 22
+	// var age = 22
+
+	// Short hand syntax
+	// age := 22
+	// name := "hi"
+
+	// Wrong
+	// var name
+	// name = "ss"
+
+	// Correct
+	// var name string
+	// name = "ss"
+
+	// var price = 10.2
+	var price float32 = 100.2
+
+	fmt.Println(price)
+}
