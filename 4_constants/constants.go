aec42b456760519328cf9e9e6126e3d96268a755
Saransh Singh <saransh2singh@gmail.com>
2024-07-14 20:20:41 +0530

    Constants

---
+++ 4_constants/constants.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// const a = 10
+
+// var game = "fifa"
+
+func main() {
+	// const name string = "goLang"
+	// const age = 30
+	// fmt.Println(name)
+
+	const (
+		port = 5000
+		host = "localhost"
+	)
+	fmt.Println(port, host)
+}
