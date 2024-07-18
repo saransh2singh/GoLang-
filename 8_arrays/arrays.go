0972c51dd732adb1df1995e82615d6fd693b06e3
Saransh Singh <saransh2singh@gmail.com>
2024-07-18 19:59:22 +0530

    Arrays

---
+++ 8_arrays/arrays.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var nums [4]int
+	// nums[0] = 1
+	// fmt.Println(nums[0])
+	// fmt.Println(nums)
+
+	// Array length
+	// fmt.Println(len(nums))
+
+	// var vals [4]bool
+	// fmt.Println(vals)
+
+	// var name [3]string
+	// name[2] = "golang"
+	// fmt.Println(name)
+
+	// To declare it in single line
+	// nums := [3]int{1, 2, 3}
+	// fmt.Println(nums)
+
+	// 2D Arrays
+	nums := [2][2]int{{3, 4}, {5, 6}}
+	fmt.Println(nums)
+}
